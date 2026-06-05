ffa80e41b97af20e0ce8dd1c58198ed8a614801b
Praveen Maurya <praveenattanagar186249@gmail.com>
2026-06-05 17:12:49 +0530

    README updated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import connectDB from './src/config/database.js';
 
 connectDB();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
