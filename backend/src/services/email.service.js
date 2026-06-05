31c2846e1338cd13382e1ebf10c219ac9f4248b8
Praveen Maurya <praveenattanagar186249@gmail.com>
2026-06-05 17:46:09 +0530

    transporter change

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,17 +1,31 @@
 import nodemailer from 'nodemailer';
 import config from '../config/config.js';
 
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         type: 'OAuth2',
+//         user: config.GOOGLE_USER,
+//         clientId: config.GOOGLE_CLIENT_ID,
+//         clientSecret: config.GOOGLE_CLIENT_SECRET,
+//         refreshToken: config.GOOGLE_REFRESH_TOKEN
+//     }
+// });
+
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-        type: 'OAuth2',
+        type: "OAuth2",
         user: config.GOOGLE_USER,
         clientId: config.GOOGLE_CLIENT_ID,
         clientSecret: config.GOOGLE_CLIENT_SECRET,
-        refreshToken: config.GOOGLE_REFRESH_TOKEN
-    }
+        refreshToken: config.GOOGLE_REFRESH_TOKEN,
+    },
+    connectionTimeout: 30000,
 });
-
 // Verify the connection configuration
 transporter.verify((error, success) => {
     if (error) {
