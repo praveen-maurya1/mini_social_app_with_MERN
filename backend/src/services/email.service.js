3784d5f15d1ecc639a6e749a0caf2b537f619818
Praveen Maurya <praveenattanagar186249@gmail.com>
2026-06-06 09:58:50 +0630

    frontend api problem solved

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,45 +1,3 @@
-// import nodemailer from 'nodemailer';
-// import config from '../config/config.js';
-
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         type: 'OAuth2',
-//         user: config.GOOGLE_USER,
-//         clientId: config.GOOGLE_CLIENT_ID,
-//         clientSecret: config.GOOGLE_CLIENT_SECRET,
-//         refreshToken: config.GOOGLE_REFRESH_TOKEN
-//     }
-// });
-
-// // Verify the connection configuration
-// transporter.verify((error, success) => {
-//     if (error) {
-//         console.error('Error connecting to email server:', error);
-//     } else {
-//         console.log('Email server is ready to send messages');
-//     }
-// });
-
-
-// // Function to send email
-// export const sendEmail = async (to, subject, text, html) => {
-//     try {
-//         const info = await transporter.sendMail({
-//             from: `"Your Name" <${config.GOOGLE_USER}>`, // sender address
-//             to, // list of receivers
-//             subject, // Subject line
-//             text, // plain text body
-//             html, // html body
-//         });
-
-//         console.log('Message sent: %s', info.messageId);
-//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-//     } catch (error) {
-//         console.error('Error sending email:', error);
-//     }
-// };
-
 
 import nodemailer from "nodemailer";
 import config from "../config/config.js";
@@ -66,23 +24,3 @@ export const sendEmail = async (to, subject, text, html) => {
     console.log("Email sent:", info.messageId);
 };
 
-// export const sendEmail = async (to, subject, text, html) => {
-//     try {
-//         const info = await transporter.sendMail({
-//             from: `"Mini Social App" <${config.SENDER_EMAIL}>`,
-//             to,
-//             subject,
-//             text,
-//             html,
-//         });
-
-//         console.log("EMAIL SENT SUCCESSFULLY");
-//         console.log(info);
-
-//         return info;
-//     } catch (error) {
-//         console.error("EMAIL FAILED");
-//         console.error(error);
-//         throw error;
-//     }
-// };
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api",
-    // baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: "http://localhost:3000/api",
+    baseURL: import.meta.env.VITE_API_URL,
 
 });
 
